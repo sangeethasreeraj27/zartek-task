871bff4f0373acbcaddb124baf6573e5d74f0d8b
sangeethasreeraj27 <73637280+sangeethasreeraj27@users.noreply.github.com>
2020-11-01 13:48:03 +0530

    new mat swipe

--- src/app/project-module/dashboard/components/dish-category/dish-category.component.ts
+++ src/app/project-module/dashboard/components/dish-category/dish-category.component.ts
@@ -123,37 +123,4 @@ export class DishCategoryComponent implements OnInit {
     }
   }
 
-
-  // second swipe
-  selectedIndex: number = 0;
-
-  selectChange(): void {
-    console.log("Selected INDEX: " + this.selectedIndex);
-  }
-
-  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
-
-  // Action triggered when user swipes
-  swipe(selectedIndex: number, action = this.SWIPE_ACTION.RIGHT) {
-    console.log("swipe");
-    console.log("number", selectedIndex);
-    console.log("action", action);
-    // Out of range
-    if (this.selectedIndex < 0 || this.selectedIndex > 1) return;
-
-    // Swipe left, next tab
-    if (action === this.SWIPE_ACTION.LEFT) {
-      const isLast = this.selectedIndex === 1;
-      this.selectedIndex = isLast ? 0 : this.selectedIndex + 1;
-      console.log("Swipe right - INDEX: " + this.selectedIndex);
-    }
-
-    // Swipe right, previous tab
-    if (action === this.SWIPE_ACTION.RIGHT) {
-      const isFirst = this.selectedIndex === 0;
-      this.selectedIndex = isFirst ? 1 : this.selectedIndex - 1;
-      console.log("Swipe left - INDEX: " + this.selectedIndex);
-    }
-  }
-
 }
